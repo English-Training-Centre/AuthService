eee9a41781051d4582d47c0b11176ddfead5b263
RamadanismaeL <ramadan.ismael02@gmail.com>
2026-01-23 17:40:40 +0200

    debug updated

--- obj/Debug/net10.0/AuthService.AssemblyInfo.cs
+++ obj/Debug/net10.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f9eb56e67b6e7266b7b92ff6bc51e9588073ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3585a7cda866a44d3f5e1e49abe71d4cb50d2f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
